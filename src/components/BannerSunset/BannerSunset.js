a77b5d060f3e5ee11b2d2c14e7aaa36bbaa05054
Logachev Nikita <logach3@gmail.com>
2024-08-01 13:30:04 +0300

    fix lint

--- src/components/BannerSunset/BannerSunset.js
+++ src/components/BannerSunset/BannerSunset.js
@@ -3,10 +3,11 @@ import { Info } from '@aragon/ui'
 
 export default function BannerSunset() {
   return (
-    <Info mode="warning" style={{marginBottom: '12px', marginTop: '12px'}}>
-      Lido DAO is sunsetting Aragon. The UI would be shutting down by the end of the year, please,
-      refer to <a href="https://vote.lido.fi">vote.lido.fi</a> and <a href="https://operators.lido.fi">operators.lido.fi</a> apps instead.
+    <Info mode="warning" style={{ marginBottom: '12px', marginTop: '12px' }}>
+      Lido DAO is sunsetting Aragon. The UI would be shutting down by the end of
+      the year, please, refer to <a href="https://vote.lido.fi">vote.lido.fi</a>
+      and <a href="https://operators.lido.fi">operators.lido.fi</a> apps
+      instead.
     </Info>
   )
 }
-
