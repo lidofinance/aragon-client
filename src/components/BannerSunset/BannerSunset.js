85193c3709ed4c65fb82d75eac1863331fb4eef4
Logachev Nikita <logach3@gmail.com>
2024-08-07 14:34:09 +0300

    Update banner text

--- src/components/BannerSunset/BannerSunset.js
+++ src/components/BannerSunset/BannerSunset.js
@@ -4,8 +4,13 @@ import { Info } from '@aragon/ui'
 export default function BannerSunset() {
   return (
     <Info mode="warning" style={{ marginBottom: '12px', marginTop: '12px' }}>
-      Lido DAO is sunsetting Aragon. The UI would be shutting down by the end of
-      the year, please, refer to <a href="https://vote.lido.fi">vote.lido.fi</a>
+      Lido contributors&nbsp;
+      <a href="https://research.lido.fi/t/discontinuation-of-aragon-ui-use/7992">
+        suggest discontinuing
+      </a>&nbsp;
+      the use of Aragon UI. The user interface will enter a ‘no support period’&nbsp;
+      until the end of the year, so please use&nbsp;
+      <a href="https://vote.lido.fi">vote.lido.fi</a>&nbsp;
       and <a href="https://operators.lido.fi">operators.lido.fi</a> apps
       instead.
     </Info>
