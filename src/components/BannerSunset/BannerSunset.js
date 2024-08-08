e50d600d5e0dd0554856cde772b506ae673f9b3f
Logachev Nikita <logach3@gmail.com>
2024-08-08 11:52:55 +0300

    fix text

--- src/components/BannerSunset/BannerSunset.js
+++ src/components/BannerSunset/BannerSunset.js
@@ -4,14 +4,15 @@ import { Info } from '@aragon/ui'
 export default function BannerSunset() {
   return (
     <Info mode="warning" style={{ marginBottom: '12px', marginTop: '12px' }}>
-      Lido contributors&nbsp;
-      <a href="https://research.lido.fi/t/discontinuation-of-aragon-ui-use/7992">
-        suggest discontinuing
-      </a>
-      &nbsp; the use of Aragon UI. The user interface will enter a ‘no support
-      period’&nbsp; until the end of the year, so please use&nbsp;
-      <a href="https://vote.lido.fi">vote.lido.fi</a>&nbsp; and&nbsp;
+      The support for Aragon UI has been discontinued. The UI is proposed to be
+      shut down by the end of 2024, so please use&nbsp;
+      <a href="https://vote.lido.fi">vote.lido.fi</a> and&nbsp;
       <a href="https://operators.lido.fi">operators.lido.fi</a> apps instead.
+      More info and discussion are on the&nbsp;
+      <a href="https://research.lido.fi/t/discontinuation-of-aragon-ui-use/7992">
+        Lido Research forum
+      </a>
+      .
     </Info>
   )
 }
